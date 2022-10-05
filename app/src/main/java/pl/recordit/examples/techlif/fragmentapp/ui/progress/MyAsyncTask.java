f73288212b020d40c16fb8ec1555639d2e9a0291
cs <siwon.cezary@gmail.com>
2022-10-05 13:51:09 +0200

    MyAsyncTask

---
+++ app/src/main/java/pl/recordit/examples/techlif/fragmentapp/ui/progress/MyAsyncTask.java
@@ -0,0 +1,39 @@
+package pl.recordit.examples.techlif.fragmentapp.ui.progress;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ProgressBar;
+
+public class MyAsyncTask extends AsyncTask<Void, Integer, String> {
+    private final ProgressBar progressBar;
+    public MyAsyncTask(ProgressBar progressBar) {
+        this.progressBar = progressBar;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        for(int i = 0; i < 500; i++){
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (i % 5 == 0) {
+                publishProgress(i / 5);
+            }
+        }
+        return "Success";
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+        progressBar.setProgress(values[0], true);
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        Log.i("APP", "End of task with " + s);
+    }
+}
